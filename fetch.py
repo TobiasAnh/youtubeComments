ae54a98ea4642d9e03a98ba719f121a348588167
Anhäuser, Tobias <anhaeuser.t@zdf.de>
2023-06-13 08:44:42 +0200

    convertign functionality into an oop approach

--- fetch.py
+++ fetch.py
@@ -3,14 +3,14 @@ import os
 import shutil
 import pandas as pd
 import json
+from pathlib import Path
 
-from src.funcs import first_key, second_key, storage_path, project_path
-from src.funcs import getChannelMetrics, getVideoIds, getVideoStatistics, getCommentsFromVideos
+from src.funcs import ytChannel
 
 # =============================================================================
 # Channel Ids overview
 # =============================================================================
-channels = pd.read_csv(project_path.joinpath("references", "channelIds.csv"))
+channels = pd.read_csv(Path(os.getcwd()).joinpath("references", "channelIds.csv"))
 print(channels)
 
 # =============================================================================
@@ -18,138 +18,127 @@ print(channels)
 # Check quota limit here: # https://console.cloud.google.com/apis
 # =============================================================================
 
-if not first_key:
-    print("no API key set")
-
-# Import Option 1 using a channelId (preferred!) 
-# Loads basic metrics and create own subfolder
-channelId = "UCa5Tb6hVZbNuD0iTnYpJdeg"
-channel_metrics, channel_foldername = getChannelMetrics(channelId, api_key_selector = first_key)
-channel_path = storage_path.joinpath(channel_foldername)
-channel_path.mkdir(exist_ok = True, parents=True)
-playlistId = channel_metrics.get("playlistId") # this list contains all videos uploaded by the channel owner
-
-# Import Option 2 (using a playlidId)
-# loads basic metrics and create own subfolder
-#playlistId = ""
-#channel_foldername = ""
-#channel_path = storage_path.joinpath(channel_foldername)
-#channel_path.mkdir(exist_ok = True)
-
-# Generates "all_videos.csv" within channel folder 
-raw_video_info = getVideoIds(playlistId, api_key_selector = first_key)
-raw_video_info = raw_video_info[~raw_video_info["videoOwnerChannelId"].isna()]
-getVideoStatistics(raw_video_info, channel_path, api_key_selector = first_key)
-
-# Import videoIds back from local storage
-all_videos = pd.read_csv(channel_path.joinpath("all_videos.csv"), 
-                         index_col = "videoId", 
-                         lineterminator="\r")
-all_videos["channel_foldername"] = channel_foldername
-
+channel = ytChannel("UCeaNCdxZcQsNMf8dkIOhLPg")
+channel.getVideoIds()
+channel.getAndSaveVideoStatistics(check_existing=False)
+channel.getAndSaveComments()
+channel.saveComments()
+""" 
 # =============================================================================
 # API comment extraction for given videoIds.
 # =============================================================================
 
 # Remove videos with no or disabled comments
-videoIds = list(all_videos.query("commentCount.notnull()") # NULL when comments are disabled
-                          .query("commentCount != 0")      # 0 comments written (includes also Livestreams)
-                          .index)
+videoIds = list(
+    all_videos.query("commentCount.notnull()")  # NULL when comments are disabled
+    .query("commentCount != 0")  # 0 comments written (includes also Livestreams)
+    .index
+)
 
 # If final file already exists, do nothing, otherwise start OR continue comment fetch.
-if (
-    ("all_comments_noSentiment.csv" in os.listdir(channel_path)) or 
-    ("all_comments_withSentiment.csv" in os.listdir(channel_path))
-   ):
-    print('--------------------')
-    print('csv file found')
-    print('Comment fetch appears finished')
-    
+if ("all_comments_noSentiment.csv" in os.listdir(channel_path)) or (
+    "all_comments_withSentiment.csv" in os.listdir(channel_path)
+):
+    print("--------------------")
+    print("csv file found")
+    print("Comment fetch appears finished")
+
 else:
     if "missing_videos.json" not in os.listdir(channel_path):
-        getCommentsFromVideos(videoIds, channel_path, api_key_selector = first_key)
-        
+        getCommentsFromVideos(videoIds, channel_path, api_key_selector=first_key)
+
     else:
         # Various reasons may lead to incomplete comment extraxtions and therefore incomplete videos
         # Sometines comments are simply not fetched (two times seems sufficent)
         runs = 2
         for i in range(runs):
             print(f"missing_videos.json found, try {i+1} ... ")
-            with open(channel_path.joinpath("missing_videos.json"), 'r') as filepath:
+            with open(channel_path.joinpath("missing_videos.json"), "r") as filepath:
                 missing_videos = json.load(filepath)
-        
-            getCommentsFromVideos(missing_videos, channel_path, api_key_selector = first_key)
-    
+
+            getCommentsFromVideos(
+                missing_videos, channel_path, api_key_selector=first_key
+            )
+
     # If still missing, most likely no quotas are left. Switch to second API key.
     if "missing_videos.json" in os.listdir(channel_path):
         print("missing_videos.json found, try with other API KEY ... ")
-        with open(channel_path.joinpath("missing_videos.json"), 'r') as filepath:
+        with open(channel_path.joinpath("missing_videos.json"), "r") as filepath:
             missing_videos = json.load(filepath)
-                
-        getCommentsFromVideos(missing_videos, channel_path, api_key_selector = second_key) 
-        
+
+        getCommentsFromVideos(missing_videos, channel_path, api_key_selector=second_key)
+
     # If STILL missing, come back after daily quota reset at 9am.
     if "missing_videos.json" in os.listdir(channel_path):
-        print("missing_videos.json found, no quotas left, no API Keys left, come back another day ... ")
+        print(
+            "missing_videos.json found, no quotas left, no API Keys left, come back another day ... "
+        )
 
 # =============================================================================
-# If fetch complete (= no missing_videos.json), files are concatenated into one csv 
-# including some augmentation of features from all_videos.csv 
+# If fetch complete (= no missing_videos.json), files are concatenated into one csv
+# including some augmentation of features from all_videos.csv
 # =============================================================================
 
 if "missing_videos.json" not in os.listdir(channel_path):
-
     video_files = os.listdir(channel_path.joinpath("tmp"))
-        
-        # Concatenate 
+
+    # Concatenate
     all_comments = pd.DataFrame()
     counter = 0
 
     for file in video_files:
-        _ = pd.read_csv(channel_path.joinpath("tmp", file), 
-                        index_col = 0, 
-                        parse_dates = ["comment_published", "comment_update"],
-                        lineterminator='\n')
-        
-        all_comments = pd.concat([all_comments, _], axis = 0)
-        
-        progress = round( counter / len(video_files), 3)
-        print(f'Fraction concatenated: {progress}')
-        
+        _ = pd.read_csv(
+            channel_path.joinpath("tmp", file),
+            index_col=0,
+            parse_dates=["comment_published", "comment_update"],
+            lineterminator="\n",
+        )
+
+        all_comments = pd.concat([all_comments, _], axis=0)
+
+        progress = round(counter / len(video_files), 3)
+        print(f"Fraction concatenated: {progress}")
+
         counter += 1
 
     # Augment video features
     video_features = all_videos[["Title", "videoOwnerChannelTitle", "publishedAt"]]
-    all_comments_aug = pd.merge(left = all_comments, 
-                                right = video_features, 
-                                how = "left", 
-                                on = "videoId")
+    all_comments_aug = pd.merge(
+        left=all_comments, right=video_features, how="left", on="videoId"
+    )
 
     del all_comments
-        
+
     # Drop unnecessary columns
-    all_comments_aug = all_comments_aug.drop(["comment_update"], axis = 1)
+    all_comments_aug = all_comments_aug.drop(["comment_update"], axis=1)
 
     # =============================================================================
     # Data export "all_comments_noSentiment.csv" (locally)
     # ... and removing tmp/ folder
     # =============================================================================
 
-    last_comment = all_comments_aug["comment_published"].max() # <- latest comment!
+    last_comment = all_comments_aug["comment_published"].max()  # <- latest comment!
     all_comments_aug.info()
-    all_comments_aug.to_csv(channel_path.joinpath("all_comments_noSentiment.csv"), lineterminator="\r")
-    print(f'all_comments_noSentiment.csv saved | last comment within data ----> {last_comment}')
+    all_comments_aug.to_csv(
+        channel_path.joinpath("all_comments_noSentiment.csv"), lineterminator="\r"
+    )
+    print(
+        f"all_comments_noSentiment.csv saved | last comment within data ----> {last_comment}"
+    )
 
     # -- User input --
-    user_input = (input(f'{channel_foldername} | {len(all_comments_aug)} comments concatenated from {len(video_files)} videos. \
-    Delete subfolder "tmp"? [Y/N]:'))
+    user_input = input(
+        f'{channel_foldername} | {len(all_comments_aug)} comments concatenated from {len(video_files)} videos. \
+    Delete subfolder "tmp"? [Y/N]:'
+    )
 
     if user_input == "Y":
         folder = channel_path.joinpath("tmp")
-        shutil.rmtree(folder)        
+        shutil.rmtree(folder)
         print("temporary folder deleted.")
     else:
         print("nothing done.")
-    
-else: 
+
+else:
     print("Comment concatenation aborted (missing_videos.json still exists)")
+ """
