945cd72fff776435ba32ed661ee8c26e03902103
Anhäuser, Tobias <anhaeuser.t@zdf.de>
2023-07-14 08:41:45 +0200

    rearrenged workflow of channel object

--- fetch.py
+++ fetch.py
@@ -7,23 +7,26 @@ from src.api import ytChannel
 # =============================================================================
 # Channel Ids overview
 # =============================================================================
-channels = pd.read_csv(Path(os.getcwd()).joinpath("references", "channelIds.csv"))
+channels = pd.read_csv(Path(os.getcwd()).joinpath("references", "channelIds.csv"), index_col="channelId")
 print(channels)
-
 # =============================================================================
 # API fetch starts with channelId (manual input)
 # Check quota limit here: # https://console.cloud.google.com/apis
 # =============================================================================
 
 # Single channel (api data fetch)
-oChannel = ytChannel("UCWKEY-aEu7gcv5ayIpgrnvg")
-oChannel.fetchVideoIds()
-oChannel.fetchAndStoreVideoStatistics(load_existing=False)
-oChannel.fetchComments(load_existing=False)
 
-# Data concatination and estimation of addtional metrics
-oChannel.concatAndSaveComments()
+for channelId in channels.index[14:15]:
+    print(f"Initiating fetch for: {channels.loc[channelId].channel_name}")
+    print()
+    oChannel = ytChannel(channelId)
+    oChannel.fetchVideoIds()
+    oChannel.fetchAndStoreVideoStatistics(load_existing=False)
+    oChannel.fetchComments(load_existing=False)
+    oChannel.concatAndSaveComments()
+
 oChannel.applyGermanSentibert()
+
 
 # Combines data from every channel found in /interim
 # When combined, new comment metrics are estimated
